dfa63283aca3555360eb56bd5117db71ecd07f19
DrewStock <andrewdstock@gmail.com>
2017-01-04 10:15:53 -0800

    added rewritten house service

--- app/src/services/house-service.js
+++ app/src/services/house-service.js
@@ -1,5 +1,32 @@
-houseService.$inject = ['$resource', 'apiUrl'];
+houseService.$inject = ['$http', 'apiUrl'];
 
-export default function houseService($resource, apiUrl) {
-    return $resource(`${apiUrl}/houses/:id`, { id: '@_id' });
+export default function houseService($http, apiUrl) {
+    return {
+        getAll() {
+            return $http.get(`${apiUrl}/houses`)
+            .then(res => res.data);
+        },
+        get(id) {
+            if(!id) return this.getAll();
+            return $http.get(`${apiUrl}/houses/${id}`)
+            .then(res => res.data);
+        },
+        add(house) {
+            return $http.post(`${apiUrl}/houses`, house)
+            .then(res => res.data);
+        },
+        join(credentials) {
+            return $http.post(`${apiUrl}/houses/house`, credentials)
+            .then(res => res.data);
+        },
+        update(id, data) {
+            return $http.put(`${apiUrl}/houses/${id}`, data)
+            .then(res => res.data);
+        },
+        remove(id) {
+            return $http.delete(`${apiUrl}/houses/${id}`)
+            .then(res => res.data);
+        },
+
+    };
 }
