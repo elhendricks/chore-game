975ddb2fdbdc9cd64aa721aadc23abf231f3ad40
elhendricks <elhendricks@gmail.com>
2017-01-04 10:57:07 -0800

    clean up extraneous todo and console.log statements

--- app/src/routes.js
+++ app/src/routes.js
@@ -20,9 +20,7 @@ export default function routes($stateProvider, $urlRouterProvider) {
                 return User.get().$promise;
             }],
 
-            //TODO: get this working
             house: ['user', 'houseService', '$transition$', (user, House) => {
-                console.log(user);
                 return House.get(user.houseId);
             }]
         }
@@ -37,11 +35,9 @@ export default function routes($stateProvider, $urlRouterProvider) {
                 return User.get().$promise;
             }],
             id: ['user', user => {
-                console.log(user);
                 return user.houseId;
             }],
             house: ['id', 'houseService', (id, House) => {
-                console.log(id);
                 return House.get(id);
             }]
         }//,
