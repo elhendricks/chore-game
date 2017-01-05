c043aecd6fddcc89a99108313b7597dc5f856919
elhendricks <elhendricks@gmail.com>
2017-01-05 12:22:28 -0800

    removed extraneous commented out code

--- app/src/components/chore-app/house-dashboard/chore-input/chore-input.js
+++ app/src/components/chore-app/house-dashboard/chore-input/chore-input.js
@@ -15,43 +15,13 @@ controller.$inject = ['userService', 'choreService'];
 function controller(User, Chore) {
 
     this.enterChore = function() {
-// TODO: Figure out how to get current date string and swap out 'Jan17' placeholders.
-// TODO: Refactor to remove any keys that are falsey. 
-// TODO: Send only the truthy names 
 
-        const arr = []
+        const arr = [];
 
         for (var key in this.data) {
             if (this.data[key] == true) {
-                arr.push(key);
-                
-        //         this.userChores = this.user.choreUnits;
-        //             // //if the user has done the chore before, 
-        //             // // add the units (time) to existing units, 
-
-        //         //  build an object to send to user
-        //         if (this.userChores && this.userChores[key] && this.userChores[key]['Jan17']) {
-        //             this.userChores[key]['Jan17'] ++;
-        //         }
-        //         // // else  (the user has never done that chore)
-        //         // // add it to user    Chores and updateUser
-        //         else if (this.userChores && this.chores[key]) {
-        //             this.userChores[key]['Jan17'] = 1;
-        //         }
-
-        //         else {
-        //             if (!this.userChores) {this.userChores = {}}
-        //             this.userChores[key] = {'Jan17': 1};
-        //         }
-
-        //         //build an object to send to chore
-
-        //         objToSend[key] = this.data[key];
-
-        //         // ideally would send: {sweepingId: true, testChoreId: true}
-                
-            }
-            
+                arr.push(key);                
+            }   
         }
         console.log(arr);
         Chore.updateMany(arr);
